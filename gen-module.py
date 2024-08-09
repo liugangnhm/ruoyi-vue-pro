54ab9cf965a16b22191128d17bf9a4c560e85721
jacky <182052514@qq.com>
2024-08-09 15:10:54 +0800

    add template sql script

--- gen-module.py
+++ gen-module.py
@@ -114,6 +114,22 @@ def generate_module_biz(parent_artifact_id, description=""):
 
     return pom_template.format(parent_artifact_id=parent_artifact_id, description=description)
 
+def gen_template_sql():
+    sql='''
+DROP table if exists sample_table;
+CREATE TABLE `sample_table` (
+    `id` bigint NOT NULL AUTO_INCREMENT COMMENT '编号',
+    `creator` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT '' COMMENT '创建者',
+    `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `updater` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT '' COMMENT '更新者',
+    `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+    `deleted` bit(1) NOT NULL DEFAULT b'0' COMMENT '是否删除',
+    `tenant_id` bigint NOT NULL DEFAULT '0' COMMENT '租户编号',
+    PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='用户组';
+'''
+    return sql
+
 
 def write_to_file(file_path, content):
     with open(file_path, "w", encoding="utf-8") as file:
@@ -154,3 +170,8 @@ os.makedirs(f"{biz_dir}/src/main/resources", exist_ok=True)
 os.makedirs(f"{biz_dir}/src/test/java", exist_ok=True)
 pom_content = generate_module_biz(artifactId)
 write_to_file(f"{biz_dir}/pom.xml", pom_content)
+
+# sql
+sql = gen_template_sql()
+os.makedirs(f"{artifactId}/sql", exist_ok=True)
+write_to_file(f"{artifactId}/sql/create.sql", sql)
